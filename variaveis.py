d553fbbb5e264450c68ca6ce1abc058427a3e6d3
Douglas Monquero <douglas.monquero@aluno.senai.br>
2026-06-03 16:50:14 -0300

    variaveis e concatenacao

---
+++ variaveis.py
@@ -0,0 +1,25 @@
+nomeDoCliente = "Antonio Jeferson"
+idadeDoCliente = 50
+cidadeDoCliente = "Maringá"
+
+print("\nNome do Cliente: " + nomeDoCliente + "\nIdade: " + str(idadeDoCliente) + "\nCidade: " + cidadeDoCliente)
+print()
+
+print("Meu nome é ", nomeDoCliente)
+print("Tenho " ,idadeDoCliente, " anos")
+print("Moro em ", cidadeDoCliente)
+
+print()
+print(f"Nome do Cliente: {nomeDoCliente}\nIdade: {idadeDoCliente}\nCidade: {cidadeDoCliente}")
+print()
+
+print(f"Nome do Cliente: {nomeDoCliente}\nIdade: {idadeDoCliente}\nCidade: {cidadeDoCliente}")
+print()
+
+texto = f"""
+Nome do Cliente: {nomeDoCliente}
+Idade: {idadeDoCliente}
+Cidade: {cidadeDoCliente}
+"""
+
+print(texto)
