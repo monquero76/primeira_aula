85652241c743ddab2098a244c3b2ce5bc26c3af0
Douglas Monquero <douglas.monquero@aluno.senai.br>
2026-06-03 17:29:03 -0300

    exercicio

--- constante.py
+++ constante.py
@@ -1,7 +1,26 @@
-DESCONTO = 10
+# DESCONTO = 10
+# preco = 100
+# print(preco * (100 - DESCONTO)/100)
 
-preco = 100
+# PI = 3.14
+# PI = 3.15
 
-print(preco * (100 - DESCONTO)/100)
+# print(PI)
 
+#Criar uma variavel para a maquina injetora de plastico H-400
+nomeInjetora = "H-400"
+#Criar uma variavel para a quandiade de ciclos por segundo
+cps = 5820
+#Criar uma variavel para a temperatura da zona 01
+temp01 = 210.5
+#Criar uma varival para saber se o sensor esta ativo TRUE
+sAtivo = True
 
+texto = f"""
+Nome da Injetora        {nomeInjetora}
+Cliclos por Segundo     {cps}
+Temperatura Zona01      {temp01}
+Sensor Ativo            {sAtivo}
+"""
+
+print(texto)
